9996091898e79461d666e34770790f92056f36ff
Raoul de Haard <raouldehaard@gmail.com>
2025-10-26 15:38:15 +0100

    update native image build plugin to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.spring") version "2.2.0"
-    id("org.graalvm.buildtools.native") version "0.10.4"
+    id("org.graalvm.buildtools.native") version "0.11.2"
 }
 
 group = "nl.rdh.github"
